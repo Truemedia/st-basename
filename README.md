121c40b6088cba7f544e3ff21380c3b4297a66b1
Wade Penistone <wadespackages@yahoo.com>
2019-03-19 15:35:33 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# st-basename
+# St basename
 Create a basename for file based on the time right now (powered by space time)
+
+## Installation
+
+NPM
+```bash
+    npm i --save st-basename
+```
+
+Yarn
+```bash
+    yarn add st-basename
+```
+
+## Usage
+```js
+  const bn = require('st-basename');
+  bn(); // YYYY-MM-DD_HH-MM-SS
+  bn('txt'); // YYYY-MM-DD_HH-MM-SS.txt
+```
--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const spacetime = require('spacetime');
 
 function bn(ext = null)
 {
-let name = spacetime.now().format('{year}-{date-pad}-{month-pad}_{hour-24}-{minute-pad}-{second-pad}');
-if (ext != null)
-{
-name += `.${ext}`;
-}
-return name;
+  let name = spacetime.now().format('{year}-{date-pad}-{month-pad}_{hour-24}-{minute-pad}-{second-pad}');
+  if (ext != null) {
+    name += `.${ext}`;
+  }
+  return name;
 }
 
 module.exports = bn;
